9d19b3217c2deae50a94fcc84690b88d96ed6e3f
Emiel Steerneman <emielsteerneman@gmail.com>
2018-08-28 01:45:29 +0200

    Added npm to install script

--- setup.sh
+++ setup.sh
@@ -1,26 +1,40 @@
+# Update
+sudo apt update
+
+# Check nodejs
 if which node > /dev/null
 then
-	echo "Nodejs is installed, skipping..."
+	echo "nodejs is installed, skipping..."
 else
 	echo "Installing nodejs"
 	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-	sudo apt update
 	sudo apt install -y nodejs
 	echo "Nodejs installed"
 fi
 
+# Check npm
+if which npm > /dev/null
+then
+	echo "npm is installed, skipping..."
+else
+	echo "Installing npm"
+	sudo apt install -y npm
+	echo "npm installed"
+fi
+
+# check git
 if which git > /dev/null
 then
 	echo "git is installed, skipping..."
 else
 	echo "Installing git"
 	sudo apt install -y git
-	echo "Git installed"
+	echo "git installed"
 fi
 
 echo "Installing nodejs packages..."
 npm install
-echo "Nodejs packages installed"
+echo "nodejs packages installed"
 
 echo "Running install script..."
 node rtt_setup.js
